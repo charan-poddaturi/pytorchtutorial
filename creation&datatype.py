3eb180c16fc552f2ed4cadaccc9a0b42de491a9c
charan-poddaturi <charan.poddaturi06@gmail.com>
2026-03-10 22:10:20 +0530

    modified

--- creation&datatype.py
+++ creation&datatype.py
@@ -14,3 +14,14 @@ torch.ones_like(x)
 
 # this gives an error because x has integers but rand functions gives the outputs as float which are between 0 1nad 1
 torch.rand_like(x) 
+
+
+# datatyes
+
+#finding datatype
+x.dtype
+
+# assigning datatype
+torch.tensor([1.0,2.0,3.0],dtype=torch.int32)
+torch.tensor([1,2,3],dtype=torch.float64)
+
