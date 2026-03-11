1397615bfa671a4414b955a5f3ac767300128f22
charan-poddaturi <charan.poddaturi06@gmail.com>
2026-03-11 18:53:41 +0530

    added more operations

--- creation&datatype.py
+++ creation&datatype.py
@@ -25,3 +25,30 @@ x.dtype
 torch.tensor([1.0,2.0,3.0],dtype=torch.int32)
 torch.tensor([1,2,3],dtype=torch.float64)
 
+
+# using to()
+x.to(torch.float32)
+
+
+# we can do all scalar operations with tensors
+
+# we can also add two tensors with same shapes
+
+# for absolute values
+torch.abs(x)
+
+# fro negative values 
+torch.neg(x)
+
+# for  rounded values
+torch.round(x)
+
+# for ceil values
+torch.ceil(x)
+
+# for floor values
+torch.floor(x)
+
+# for clamp(values under 2 becomes 2 and values above 3 becomes 3)
+torch.clamp(x, min=2, max=3)
+
